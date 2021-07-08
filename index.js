2f47f6d87e3fb86a64b0d1129db238cfa3bf02dd
AshaSaha-27 <57867179+AshaSaha-27@users.noreply.github.com>
2021-07-08 11:54:39 -0400

    created function in index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+// inside the $(document).ready() event handler, creating a new function
+  function displayTime(){
+//create a variable called currentTime and set its value to a new Date object.
+  var currentTime= new Date();
+  var hours= currentTime.getHours();
+  var minutes= currentTime.getMinutes();
+  var seconds = currentTime.getSeconds();
+  // This gets a "handle" to the clock div in our HTML
+
+  }
+});
